4e3874e63f3b117933dc33ddf8c5bcadbf2de036
Ford Arad <calinutzuu@yahoo.com>
2019-04-09 12:25:16 +0300

    up

--- application/views/home.php
+++ application/views/home.php
@@ -3,7 +3,7 @@
 <section class="grid_3 carousel">
 <div id="hpCarousel" role="listbox">
 <!--Rendering the Image Panel-->
-
+<!--
 <article role="option">
     <a class="carImg" href="#img1"><img src="/assets/img/vehicule_comercial_19.jpg"
                                         alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
@@ -57,7 +57,36 @@
         </div>
     </div>
 </article>
+ -->
 
+<article role="option">
+    <a class="carImg" href="#img1"><img src="/assets/img/kuga_apr_19.jpg"
+                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
+                                        role="presentation"></a>
+
+    <div class="innerBox posMiddleLeft WhiteText">
+        <br><br><br><br>
+        <div class="">
+            <h2 class="bigHeading02">
+                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
+                         style="font-family:FordAntennaBlack;"> </span></span>
+            </h2>
+
+            <p class="captionTxt">
+                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
+            </p>
+
+            <div class="btnwrapper">
+                <div class="btnLink light">
+                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/kuga/promotii/persoane-fizice">Afla mai multe</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</article>
 <article role="option">
     <a class="carImg" href="#img1"><img src="/assets/img/ecosport_1.jpg"
                                         alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
@@ -85,53 +114,7 @@
     </div>
 </article>
 
-<article role="option">
-    <a class="carImg" href="#img1"><img src="/assets/img/kuga_1.jpg"
-                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
-                                        role="presentation"></a>
 
-    <div class="innerBox posMiddleLeft WhiteText">
-        <br><br><br><br>
-        <div class="">
-            <h2 class="bigHeading02">
-                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
-                         style="font-family:FordAntennaBlack;"> </span></span>
-            </h2>
-
-            <p class="captionTxt">
-                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
-            </p>
-
-            <div class="btnwrapper">
-                <div class="btnLink light">
-                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/kuga/promotii/persoane-juridice">Oferta persoane fizice</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="innerBox posTopLeft WhiteText">
-        <br><br><br><br>
-        <div class="">
-            <h2 class="bigHeading02">
-                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
-                         style="font-family:FordAntennaBlack;"> </span></span>
-            </h2>
-
-            <p class="captionTxt">
-                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
-            </p>
-
-            <div class="btnwrapper">
-                <div class="btnLink light">
-                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/kuga/promotii/persoane-fizice">Oferta persoane juridice</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</article>
 
 <article role="option">
     <a class="carImg" href="#img1"><img src="/assets/img/service/motor_primavara.jpg"
