9f79e2e6ec041c954900db6ec0a6a79eafe4956e
Ford Arad <calinutzuu@yahoo.com>
2019-07-01 15:59:17 +0300

    update

--- application/views/home.php
+++ application/views/home.php
@@ -58,7 +58,7 @@
     </div>
 </article>
  -->
-
+<!--
 <article role="option">
     <a class="carImg" href="#img1"><img src="/assets/img/KUGA_IUN.jpg"
                                         alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
@@ -175,7 +175,38 @@
     </div>
 </article>
 
+-->
 
+
+
+<article role="option">
+    <a class="carImg" href="#img1"><img src="/assets/img/ford_puma.jpg"
+                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
+                                        role="presentation"></a>
+
+    <div class="innerBox posTopLeft WhiteText">
+        <br><br><br><br>
+        <div class="">
+            <h2 class="bigHeading02">
+                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
+                         style="font-family:FordAntennaBlack;"> </span></span>
+            </h2>
+
+            <p class="captionTxt">
+                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
+            </p>
+
+            <div class="btnwrapper">
+                <div class="btnLink light">
+                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/noul-puma">Afla mai multe</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</article>
 
 <article role="option">
     <a class="carImg" href="#img1"><img src="/assets/img/service/motor_primavara.jpg"
