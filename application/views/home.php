213b8df8c69e904d6671eb41d68dca1bacdb53dc
Ford Arad <calinutzuu@yahoo.com>
2019-10-25 09:32:24 +0300

    update

--- application/views/home.php
+++ application/views/home.php
@@ -177,34 +177,7 @@
 
 -->
 
-<article role="option">
-    <a class="carImg" href="#img1"><img src="/assets/img/rabla_2019.jpg"
-                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
-                                        role="presentation"></a>
 
-    <div class="innerBox posBottomLeft WhiteText">
-        <br><br><br><br>
-        <div class="">
-            <h2 class="bigHeading02">
-                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
-                         style="font-family:FordAntennaBlack;"> </span></span>
-            </h2>
-
-            <p class="captionTxt">
-                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
-            </p>
-
-            <div class="btnwrapper">
-                <div class="btnLink light">
-                    <a role="button" class="om_ca_in" href="https://www.ford.ro/cumpara/alege-modelul/promotii-ford/programul-ford-rabla">Afla mai multe</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-
-</article>
 
 
 <article role="option">
@@ -236,34 +209,7 @@
 
 </article>
 
-<article role="option">
-    <a class="carImg" href="#img1"><img src="/assets/img/kuga_iulie.jpg"
-                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
-                                        role="presentation"></a>
 
-    <div class="innerBox posTopRight WhiteText">
-        <br><br><br><br>
-        <div class="">
-            <h2 class="bigHeading02">
-                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
-                         style="font-family:FordAntennaBlack;"> </span></span>
-            </h2>
-
-            <p class="captionTxt">
-                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
-            </p>
-
-            <div class="btnwrapper">
-                <div class="btnLink light">
-                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/kuga/promotii/persoane-fizice">Rezerva oferta acuma</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-
-</article>
 
 
 <article role="option">
