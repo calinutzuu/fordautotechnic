c48114a1800adc8e0e2526bcc6d2b6dab1142273
Ford Arad <calinutzuu@yahoo.com>
2019-11-01 13:46:50 +0200

    update nov1

--- application/views/home.php
+++ application/views/home.php
@@ -209,6 +209,35 @@
 
 </article>
 
+<article role="option">
+    <a class="carImg" href="#img1"><img src="/assets/img/kuga_nov_2.jpg"
+                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
+                                        role="presentation"></a>
+
+    <div class="innerBox posBottomLeft WhiteText">
+        <br><br><br><br>
+        <div class="">
+            <h2 class="bigHeading02">
+                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
+                         style="font-family:FordAntennaBlack;"> </span></span>
+            </h2>
+
+            <p class="captionTxt">
+                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
+            </p>
+
+            <div class="btnwrapper">
+                <div class="btnLink light">
+                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/kuga/promotii/persoane-fizice">Afla mai multe</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</article>
+
 
 
 
