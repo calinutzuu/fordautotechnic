2e8e971a9d23d7aecfb3e11068b0be73ee91a7d2
Ford Arad <calinutzuu@yahoo.com>
2020-07-20 11:14:41 +0300

    update kuga

--- application/views/home.php
+++ application/views/home.php
@@ -266,7 +266,63 @@
 
 </article>
 
+<article role="option">
+    <a class="carImg" href="#img1"><img src="/assets/img/kuga_20.jpg"
+                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
+                                        role="presentation"></a>
 
+    <div class="innerBox posTopLeft WhiteText">
+        <br><br><br><br>
+        <div class="">
+            <h2 class="bigHeading02">
+                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
+                         style="font-family:FordAntennaBlack;"> </span></span>
+            </h2>
+
+            <p class="captionTxt">
+                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
+            </p>
+
+            <div class="btnwrapper">
+                <div class="btnLink light">
+                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/kuga/promotii/persoane-juridice">Afla mai multe</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</article>
+
+<article role="option">
+    <a class="carImg" href="#img1"><img src="/assets/img/kuga_20.jpg"
+                                        alt="&lt;span style=&quot;font-size:28px;&quot;&gt;&lt;span style=&quot;font-family:FordAntennaRegular;&quot;&gt; &lt;/span&gt;&lt;span style=&quot;font-family:FordAntennaBlack;&quot;&gt; &lt;/span&gt;&lt;/span&gt;"
+                                        role="presentation"></a>
+
+    <div class="innerBox posTopLeft WhiteText">
+        <br><br><br><br>
+        <div class="">
+            <h2 class="bigHeading02">
+                 <span style="font-size:28px;"><span style="font-family:FordAntennaRegular;"> </span><span
+                         style="font-family:FordAntennaBlack;"> </span></span>
+            </h2>
+
+            <p class="captionTxt">
+                <span style="font-size:18px;"><span style="font-family:FordAntennaRegular;"> </span></span>
+            </p>
+
+            <div class="btnwrapper">
+                <div class="btnLink light">
+                    <a role="button" class="om_ca_in" href="https://www.ford.ro/autoturisme/kuga/promotii/persoane-juridice">Afla mai multe</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</article>
 
 
 <!--
--- application/views/layouts/includes/footer.php
+++ application/views/layouts/includes/footer.php
@@ -1,115 +1,118 @@
 <!-- Footer section-->
 <footer>
-<nav id="footerNav" role="navigation">
-<div class="listColumn">
+    <nav id="footerNav" role="navigation">
+        <div class="listColumn">
 
-    <dl>
-        <dt>Autoturisme</dt>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/ka-plus">Noul Ka+</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/noul-fiesta"> Noul Fiesta</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Fiesta"> Noul Fiesta ST</a></h4></dd>
-             <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulB-MAX"> B-MAX</a></h4></dd> -- >
-      <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-EcoSport"> EcoSport</a></h4></dd> -->
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulEcosport"> EcoSport</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Focus"> Focus</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/focus-2018"> Noul Focus</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NewFocusST"> The New Focus ST</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/FocusRS"> Focus RS</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulC-MAX"> Noul C-MAX</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul_Kuga"> Noul Kuga</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Mondeo"> Mondeo</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/mondeo-vignale"> Mondeo Vignale</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulMustang"> Noul Mustang</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-S-MAX"> S-MAX</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/s-max-vignale"> S-Max Vignale</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-FordEdge"> Edge</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulGalaxy"> Noul Galaxy</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulTourneoConnect"> Noul Tourneo Connect</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-Ford-Tourneo-Courier"> Noul Ford Tourneo Courier</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulFordGT"> Noul Ford GT</a></h4></dd>
-  <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/KineticDesign"> Ford kinetic Design</a></h4></dd> -->
-  <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Tehnologii"> Tehnologii</a></h4></dd> -->
-  <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/mici"> Masini mici</a></h4></dd> -->
-  <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/defamilie"> Masini de familie</a></h4></dd> -->
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/st-line"> ST-Line</a></h4></dd>
-   <!-- <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/FutureVehicles"> </a></h4></dd> -->
-    </dl>
-</div>
-<div class="listColumn">
-    <dl>
-        <dt>Vehicule comerciale</dt>
+            <dl>
+                <dt>Autoturisme</dt>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/ka-plus">Noul Ka+</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/noul-fiesta"> Noul Fiesta</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Fiesta"> Noul Fiesta ST</a></h4></dd>
+                <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulB-MAX"> B-MAX</a></h4></dd> -- >
+                <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-EcoSport"> EcoSport</a></h4></dd> -->
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulEcosport"> EcoSport</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Focus"> Focus</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/focus-2018"> Noul Focus</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NewFocusST"> The New Focus ST</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/FocusRS"> Focus RS</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulC-MAX"> Noul C-MAX</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul_Kuga"> Noul Kuga</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Mondeo"> Mondeo</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/mondeo-vignale"> Mondeo Vignale</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulMustang"> Noul Mustang</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-S-MAX"> S-MAX</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/s-max-vignale"> S-Max Vignale</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-FordEdge"> Edge</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulGalaxy"> Noul Galaxy</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulTourneoConnect"> Noul Tourneo Connect</a>
+                    </h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Noul-Ford-Tourneo-Courier"> Noul Ford Tourneo
+                            Courier</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/NoulFordGT"> Noul Ford GT</a></h4></dd>
+                <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/KineticDesign"> Ford kinetic Design</a></h4></dd> -->
+                <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/Tehnologii"> Tehnologii</a></h4></dd> -->
+                <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/mici"> Masini mici</a></h4></dd> -->
+                <!--  <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/defamilie"> Masini de familie</a></h4></dd> -->
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/st-line"> ST-Line</a></h4></dd>
+                <!-- <dd><h4><a class='sbe om_fo_ff_li ' href="/Autoturisme/FutureVehicles"> </a></h4></dd> -->
+            </dl>
+        </div>
+        <div class="listColumn">
+            <dl>
+                <dt>Vehicule comerciale</dt>
 
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/Transit-Courier"> Transit Courier</a>
-            </h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/TransitConnect">  Transit Connect</a>
-            </h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/NoulTransitCustom">  Transit
-                    Custom</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/NoulTourneoCustom"> Tourneo Custom</a></h4>
-        </dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="#">  Transit
-                    Autoşasiu</a></h4></dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/Transit2014"> Ford Transit</a></h4>
-        </dd>
-        <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/NoulRanger"> Noul Ranger</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/Transit-Courier"> Transit Courier</a>
+                    </h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/TransitConnect"> Transit Connect</a>
+                    </h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/NoulTransitCustom"> Transit
+                            Custom</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/NoulTourneoCustom"> Tourneo Custom</a>
+                    </h4>
+                </dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="#"> Transit
+                            Autoşasiu</a></h4></dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/Transit2014"> Ford Transit</a></h4>
+                </dd>
+                <dd><h4><a class='sbe om_fo_ff_li ' href="/VehiculeComerciale/NoulRanger"> Noul Ranger</a></h4></dd>
 
 
-    </dl>
-</div>
-<div class="listColumn">
-    <dl>
-        <dt>Ford Service</dt>
-        <dd>
-            <a class='om_fo_ff_li ' href="/FordService/Motorcraft">
-                Promoţii service</a>
-        </dd>
-        <dd>
-            <a class='om_fo_ff_li ' href="/FordService/Accesorii">
-                Accesorii</a>
-        </dd>
-        <dd>
-            <a class='om_fo_ff_li ' href="/FordService/GarantiiSiRevizii">
-                Garanţii şi revizii Ford</a>
-        </dd>
-        <dd>
-            <a class='om_fo_ff_li ' href="/FordService/FordAsistenta12">
-                FordAsistenta 12</a>
-        </dd>
-        <dd>
-            <!--    <a class='om_fo_ff_li ' href="/FordService/ManualeDeUtilizare">
-                    Manuale de utilizare</a>  -->
-        </dd>
-        <dd>
-            <a class='om_fo_ff_li ' href="/FordService/Ford-Lifestyle-Collection">
-                Ford Lifestyle Collection</a>
-        </dd>
-        <dd>
-            <!--  <a class='om_fo_ff_li ' href="/Promotii/PromotiiService/BlueOval">
-                  Înlocuire Blue Oval</a> -->
-        </dd>
-        <dd>
-            <a class='om_fo_ff_li ' href="/FordService/IntrebariFrecvente">
-                Întrebări frecvente</a>
-        </dd>
-    </dl>
-</div>
+            </dl>
+        </div>
+        <div class="listColumn">
+            <dl>
+                <dt>Ford Service</dt>
+                <dd>
+                    <a class='om_fo_ff_li ' href="/FordService/Motorcraft">
+                        Promoţii service</a>
+                </dd>
+                <dd>
+                    <a class='om_fo_ff_li ' href="/FordService/Accesorii">
+                        Accesorii</a>
+                </dd>
+                <dd>
+                    <a class='om_fo_ff_li ' href="/FordService/GarantiiSiRevizii">
+                        Garanţii şi revizii Ford</a>
+                </dd>
+                <dd>
+                    <a class='om_fo_ff_li ' href="/FordService/FordAsistenta12">
+                        FordAsistenta 12</a>
+                </dd>
+                <dd>
+                    <!--    <a class='om_fo_ff_li ' href="/FordService/ManualeDeUtilizare">
+                            Manuale de utilizare</a>  -->
+                </dd>
+                <dd>
+                    <a class='om_fo_ff_li ' href="/FordService/Ford-Lifestyle-Collection">
+                        Ford Lifestyle Collection</a>
+                </dd>
+                <dd>
+                    <!--  <a class='om_fo_ff_li ' href="/Promotii/PromotiiService/BlueOval">
+                          Înlocuire Blue Oval</a> -->
+                </dd>
+                <dd>
+                    <a class='om_fo_ff_li ' href="/FordService/IntrebariFrecvente">
+                        Întrebări frecvente</a>
+                </dd>
+            </dl>
+        </div>
 
-<div class="listColumn">
-    <dl class='social'>
-        <dt>URMĂREŞTE-NE</dt>
-        <dd>
-            <a class='om_fo_ff_li ' target="_new" href="https://www.facebook.com/FordAutoTechnic">
-                <span style="background-image: url('<?= base_url('assets/img/1214484916183.png') ?>')"></span>
-                Facebook</a>
-                Facebook</a>
-        </dd>
+        <div class="listColumn">
+            <dl class='social'>
+                <dt>URMĂREŞTE-NE</dt>
+                <dd>
+                    <a class='om_fo_ff_li ' target="_new" href="https://www.facebook.com/FordAutoTechnic">
+                        <span style="background-image: url('<?= base_url('assets/img/1214484916183.png') ?>')"></span>
+                        Facebook</a>
+                    Facebook</a>
+                </dd>
 
-    </dl>
-</div>
-<div class="listColumn">
-    <dl>
-        <dt>LINKURI RAPIDE</dt>
-     <!--   <dd>
+            </dl>
+        </div>
+        <div class="listColumn">
+            <dl>
+                <dt>LINKURI RAPIDE</dt>
+                <!--   <dd>
             <a class='sbe om_fo_ff_SBEI ' href="/SBE/VreauSaFiuInformata">
                 <span style="background-image: url('<?= base_url('/assets/img/1214481759413.png') ?>')"></span>
                 Vreau să fiu informat(ă)</a>
@@ -148,22 +151,25 @@
                 </dd>
             </dl>
         </dd>  -->
-        <dd>
-            <a class='sbe om_fo_ff_li ' href="https://www.ford.ro/service/masina-mea/programare-service#!/vehicle-step?dc=10300">
-                <span style="background-image: url('<?= base_url('assets/img/blob/1214485115013.png') ?>')"></span>
-                Programari Service</a>
-        </dd>
-        <dd>
-            <a class='sbe om_fo_ff_li ' href="/contact/contact">
-                <span style="background-image: url('<?= base_url('assets/img/blob/1214485115013.png') ?>')"></span>
-                Contact</a>
-        </dd>
-    </dl>
-</div>
-</nav>
-<nav id="footerLinks">
+                <dd>
+                    <a class='sbe om_fo_ff_li '
+                       href="https://www.ford.ro/service/masina-mea/programare-service#!/vehicle-step?dc=10300">
+                        <span
+                            style="background-image: url('<?= base_url('assets/img/blob/1214485115013.png') ?>')"></span>
+                        Programari Service</a>
+                </dd>
+                <dd>
+                    <a class='sbe om_fo_ff_li ' href="/contact/contact">
+                        <span
+                            style="background-image: url('<?= base_url('assets/img/blob/1214485115013.png') ?>')"></span>
+                        Contact</a>
+                </dd>
+            </dl>
+        </div>
+    </nav>
+    <nav id="footerLinks">
 
-</nav>
+    </nav>
 </footer>
 <!--<END_EP2_FOOTER >-->
 
